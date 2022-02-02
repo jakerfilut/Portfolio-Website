caa12343201d2187552597614bfab0577941d9d1
Jake <jakerfilut@gmail.com>
2022-02-01 20:52:26 -0700

    changed pic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/NavBar/Navbar.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/HomePage/Home";
 import Tech from "./components/TechPage/Tech";
-import Projects from "./components/Projects/Project";
 import Contact from "./components/ContactMe/Mailer";
 import NewProject from "./components/NewProject/NewPoject";
 import Footer from "./components/Footer/Footer";

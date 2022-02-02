8494dc63a9339654037c183068d5627d7078cbb7
Jake <jakerfilut@gmail.com>
2022-02-01 21:21:28 -0700

    changed pic

--- src/components/HomePage/Data.js
+++ src/components/HomePage/Data.js
@@ -8,6 +8,6 @@ export const homeObjOne = {
     "I’m a software engineer from Dever, with a passion for creating and problem solving. If you want to know more about me, my work or if you're a Nigerian Prince who wants to offer me a lot of money, feel free to contact me.",
   buttonLabel: "Contact Me",
   imgStart: "",
-  img: "/images/temppic.jpeg",
+  img: "/images/temppic.jpg",
   alt: "Jake Filut",
 };
